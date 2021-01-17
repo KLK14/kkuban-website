5a2d4b48537aaf543afea0718a4b4f5e8adf78b8
Katharina Kuban <katharina.kuban@web.de>
2021-01-17 14:23:42 +0100

    project setup

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  siteMetadata: {
+    title: "KKuban",
+  },
+  plugins: [
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-typescript",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const IndexPage: React.FunctionComponent = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>Hello</h1>
+    </main>
+  );
+};
+
+export default IndexPage;
